5b3b50c88a21467e0e187b19a6b99d1f138711cb
rattus-aristarchus <lankin.m.a@gmail.com>
2025-10-09 10:01:07 +0300

    dummy manual test

--- test/welcome_test.py
+++ test/welcome_test.py
@@ -6,3 +6,9 @@ from src.test_cases_as_code import get_message
 def test_test_cases_as_code():
     with allure.step("Welcome to Allure Report!"):
         assert get_message() == "Hello from test-cases-as-code!"
+
+@allure.title("Demo manual test")
+@allure.label("manual", "true")
+def test_demo_manual():
+    with allure.step("First step"):
+        pass
