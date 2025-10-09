8f84f7e0a476be93dc74f102183184f47d63fb0f
rattus-aristarchus <lankin.m.a@gmail.com>
2025-10-09 12:06:10 +0300

    custom step

--- test/welcome_test.py
+++ test/welcome_test.py
@@ -7,8 +7,14 @@ def test_test_cases_as_code():
     with allure.step("Welcome to Allure Report!"):
         assert get_message() == "Hello from test-cases-as-code!"
 
-@allure.title("Demo manual test")
+@allure.title("Демонстрационный ручной тест")
 @allure.manual
 def test_demo_manual():
-    with allure.step("First step"):
+    with allure.step("Демонстрационный шаг"):
         pass
+    custom_step()
+
+
+@allure.step("Пользовательский шаг")
+def custom_step():
+    pass
