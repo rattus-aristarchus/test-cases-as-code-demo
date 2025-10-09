6b12f1868e89d242bef6ad0e7786d3ec52ded0dc
rattus-aristarchus <lankin.m.a@gmail.com>
2025-10-09 12:52:23 +0300

    annotations

--- test/welcome_test.py
+++ test/welcome_test.py
@@ -1,4 +1,5 @@
 import allure
+from allure_commons.types import LabelType
 
 from src.test_cases_as_code import get_message
 
@@ -8,6 +9,11 @@ def test_test_cases_as_code():
         assert get_message() == "Hello from test-cases-as-code!"
 
 @allure.title("Демонстрационный ручной тест")
+@allure.story("Story")
+@allure.feature("Feature")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("example")
+@allure.label(LabelType.OWNER, "mlankin")
 @allure.manual(True)
 def test_demo_manual():
     with allure.step("Демонстрационный шаг"):
