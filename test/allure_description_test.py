b50da3ca5eda2abedd5fad8aa99f34754a1ffa22
rattus-aristarchus <lankin.m.a@gmail.com>
2025-10-13 11:21:02 +0300

    some new tests

---
+++ test/allure_description_test.py
@@ -0,0 +1,42 @@
+import allure
+
+
+def test_docstring_description():
+    """
+    This is a test docstring
+    It's will be shown as an allure test description
+    Scenario:
+     - pass
+    """
+    pass
+
+@allure.epic("Allure TestOps")
+@allure.severity("Critical")
+@allure.manual(True)
+@allure.description(
+    """
+This is test description from decorator\n
+Scenario:
+  - pass
+  """)
+def test_decorated_description():
+    pass
+@allure.epic("Allure TestOps")
+@allure.severity("Major")
+@allure.description_html("""
+This is <b>HTML</b> test description
+<h2>Scenario:</h2>
+<ul>
+<li>pass</li>
+</ul>
+""")
+def test_decorated_html_description():
+    pass
+
+
+def test_dynamic_description():
+    """
+    Initial test description
+    """
+    allure.dynamic.description(test_dynamic_description.__doc__ +
+                               f"\n This is dynamic description part: {2 + 2}")
